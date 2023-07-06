a5ed15f026749f0f0721899da6bb42f3aa2b25d6
Andreas Dutzler <a.dutzler@gmail.com>
2023-07-07 01:15:00 +0200

    Rebase `identity4()` on `cdya()`

--- ttb/libidentity.f
+++ ttb/libidentity.f
@@ -31,12 +31,9 @@
         
         type(Tensor2), intent(in) :: T
         type(Tensor4) :: ident_4
-        integer :: i,j,k,l
         
-        ident_4%abcd = 0.d0
-        forall(i=1:3,j=1:3,k=1:3,l=1:3) ident_4%abcd(i,j,k,l) = 
-     *       1./2.*(T%ab(i,k)*T%ab(j,l)+T%ab(i,l)*T%ab(j,k))
-        
+        ident_4 = T.cdya.T
+
        end function ident_4
        
        function ident_4s(T)
@@ -44,34 +41,7 @@
         
         type(Tensor2s), intent(in) :: T
         type(Tensor4s) :: ident_4s
-        integer :: i,j,k,l
-        integer, dimension(3,3) :: i6
-        
-        i6 = reshape( (/1,4,6, 4,2,5, 6,5,3/), (/3, 3/) )
-        
-        ident_4s%a6b6 = 0.d0
-        !do i=1,6
-        ! ident_4s%a6b6(i,i) = 1.d0
-        !enddo
-        
-        do i=1,3
-         do j=1,3
-         if (i.le.j) then
-          do k=1,3
-           do l=1,3
-           if (k.le.l) then
-            ident_4s%a6b6(i6(i,j),i6(k,l))
-     *       = ( T%a6(i6(i,k)) * T%a6(i6(j,l))
-     *          +T%a6(i6(i,l)) * T%a6(i6(j,k)) )/2.d0
-            if (i.ne.j) then
-             ident_4s%a6b6(i6(i,j),i6(k,l)) = 
-     *        2.d0*ident_4s%a6b6(i6(i,j),i6(k,l))
-            endif
-           end if
-           end do
-          end do
-         end if
-         end do
-        end do
+
+        ident_4s = T.cdya.T
         
        end function ident_4s
