13197e25bbb8ce75e1a782911e3d43ff7a38b4c0
JorgeG94 <jorgegalvez1694@gmail.com>
2025-02-08 19:33:11 +1100

    add support for more compilers, needs to define flags...

--- app/main.f90
+++ app/main.f90
@@ -1,5 +1,14 @@
 program test
    use Tensor
+   implicit none
+   type(Tensor1) :: a, b, c
+
+   a%a = 2.0_dp
+   b%a = 1.0_dp
+   c%a = 0.0_dp
+
+   c = a + b
+   print *, c
 
    call print_banner()
 end program test
